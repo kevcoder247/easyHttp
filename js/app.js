7b867cdbe9afe9e04ea886775cf41fce773b71f2
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2022-09-12 11:40:47 -0400

    Http delete request

--- js/app.js
+++ js/app.js
@@ -25,10 +25,20 @@ const data = {
 // });
 
 //Update Put
-http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post){
-    if(err){
-     console.log(err);
-    }else{
-     console.log(post);
-    }
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post){
+//     if(err){
+//      console.log(err);
+//     }else{
+//      console.log(post);
+//     }
+// });
+
+
+//Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(response);
+  }
 });
--- js/easthttp.js
+++ js/easthttp.js
@@ -14,7 +14,6 @@ easyHTTP.prototype.get = function(url, callback) {
         callback('Error: ' + self.http.status);
       }
     }
-  
     this.http.send();
   }
 
@@ -27,9 +26,6 @@ easyHTTP.prototype.post = function(url, data, callback){
     this.http.onload = function() {
         callback(null, self.http.responseText);
     }
-
-
-
     this.http.send(JSON.stringify(data));
 }
 
@@ -42,12 +38,22 @@ easyHTTP.prototype.put = function(url, data, callback){
     this.http.onload = function() {
         callback(null, self.http.responseText);
     }
-
-
-
     this.http.send(JSON.stringify(data));
 }
 
 
 
 //Make an HTTP Delete Request
+easyHTTP.prototype.delete = function(url, callback) {
+    this.http.open('DELETE', url, true);
+  
+    let self = this;
+    this.http.onload = function() {
+      if(self.http.status === 200) {
+        callback(null, 'Post Deleted');
+      } else {
+        callback('Error: ' + self.http.status);
+      }
+    }
+    this.http.send();
+  }
